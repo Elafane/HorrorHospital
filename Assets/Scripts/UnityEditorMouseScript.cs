314c081dbf2583fd98a26d4fe1d2760c3334579a
MaWu <mawu-games@t-online.de>
2015-06-06 21:38:27 +0200

    New WoodenFloor

---
+++ Assets/Scripts/UnityEditorMouseScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityEditorMouseScript : MonoBehaviour {
+
+    bool cursorLocked;
+
+	// Use this for initialization
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        cursorLocked = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        if (Input.GetButtonDown("Cancel"))
+		{
+            if (Application.isEditor)
+            {
+                if (cursorLocked)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                    Cursor.visible = true;
+                    cursorLocked = false;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    cursorLocked = true;
+                }
+            }
+		}
+	}
+
+
+
+}
